8be0a168943b2462c30e8f3caa157a61a7851fda
daodaoawaker <471229015@qq.com>
2022-04-04 12:01:35 +0800

    update 2022/04/04

--- segmentron/data/dataloader/Ego2Hands.py
+++ segmentron/data/dataloader/Ego2Hands.py
@@ -11,6 +11,9 @@ from .Ego2Hands_utils import *
 
 
 
+LEFT_IDX = 1
+RIGHT_IDX = 2
+
 class Ego2HandsData(BaseDataset):
     def __init__(self, transform=None, mode='Train'):
         super(Ego2HandsData, self).__init__(transform=transform, mode=mode)
@@ -22,32 +25,142 @@ class Ego2HandsData(BaseDataset):
 
         self.sample_path = get_sample_path_ego2hands(self.datasets, mode)
         self.sample_list = self.read_samples()
+        self.bg_list = read_bg_data(os.path.join(self.root, Cfg.DATASET.NAME, Cfg.DATASET.BACKGROUND))
+
+        self.input_edge = Cfg.MODEL.INPUT_EDGE
+        self.valid_hand_seg_threshold = 5000
 
     def __getitem__(self, index):
         input_size = [int(i) for i in Cfg.INPUT.IMAGE_SIZE]
 
-        # letf hand
+        # --------------------------------------- letf hand
         id_l = random.randint(0, self.__len__() - 1)
-        left_src = cv2.imread(self.sample_list[id_l], cv2.IMREAD_UNCHANGED)
-        shape = left_src.shape
+        left_src = cv2.imread(self.sample_list[id_l]['image_path'], cv2.IMREAD_UNCHANGED)
+        assert left_src != None, f"Error, image not found: {self.sample_list[id_l]['image_path']}"
 
         left_img = left_src.astype(np.float32)
-        left_img = ReScale(input_size, cv2.INTER_CUBIC)(left_img)
+        left_img = ReScale(input_size, cv2.INTER_AREA)(left_img)
         left_img = Flip('Horizontal')(left_img)
+        left_img_orig = left_img.copy()
 
         left_seg = left_img[:, :, -1] > 128
-        left_energy = cv2.imread(self.sample_list[id_l], 0).astype(np.float32)
-        left_energy = ReScale(input_size, cv2.INTER_NEAREST)(left_energy)
+        left_energy = cv2.imread(self.sample_list[id_l]['label_path'], 0)
+        left_energy = ReScale(input_size, cv2.INTER_AREA)(left_energy)
         left_energy = Flip('Horizontal')(left_energy)
+        left_energy = left_energy.astype(np.float32) / 255.
 
+        # augmentation
         left_img, left_seg, left_energy = random_translation(left_img, left_seg, left_energy)
+        left_img = random_brightness(left_img, left_seg)
+        left_img = random_smoothness(left_img)
 
+        # --------------------------------------- right hand
+        id_r = random.randint(0, self.__len__() - 1)
+        right_src = cv2.imread(self.sample_list[id_r]['image_path'], cv2.IMREAD_UNCHANGED)
+        assert right_src != None, f"Error, image not found: {self.sample_list[id_r]['image_path']}"
+
+        right_img = right_img.astype(np.float32)
+        right_img = ReScale(input_size, cv2.INTER_AREA)(right_img)
+        right_img_orig = right_img.copy()
+
+        right_seg = right_img[:, :, -1] > 128
+        right_energy = cv2.imread(self.sample_list[id_r]['label_path'], cv2.IMREAD_GRAYSCALE)
+        right_energy = ReScale(input_size, cv2.INTER_AREA)(right_energy)
+        right_energy = right_energy.astype(np.float32) / 255.
+
+        # augmentation
+        right_img, right_seg, right_energy = random_translation(right_img, right_seg, right_energy)
+        right_img = random_brightness(right_img, right_seg)
+        right_img = random_smoothness(right_img)
+
+        # --------------------------------------- background images
+        bg_img = None
+        while (bg_img is None):
+            id_b = random.randint(0, len(self.bg_list) - 1)
+            bg_img = cv2.imread(self.bg_list[id_b], cv2.IMREAD_COLOR).astype(np.float32)
+            bg_img = random_bg_augment(bg_img)
+            bg_img = random_smoothness(bg_img)
+
+        # --------------------------------------- merge hands
+        merge_mode = random.randint(0, 9)
+        if merge_mode < 8:
+            if np.sum(left_energy) > np.sum(right_energy):
+                merge_mode = 0  # left hand first
+            else:
+                merge_mode = 4  # right hand first
+
+        if merge_mode < 4:
+            # left hand top, right hand bottom
+            img_real, bg_img_resized = merge_hands(left_img, right_img, bg_img)
+            img_real_orig, _ = merge_hands(left_img_orig, right_img_orig, bg_img_resized, bg_resize=False)
+            
+            seg_real = np.zeros((img_real.shape[:2]), dtype=np.uint8)
+            seg_real[right_seg] = RIGHT_IDX
+            seg_real[left_seg] = LEFT_IDX
+            # if right-hand's size is insufficient, drop it
+            right_mask = seg_real == RIGHT_IDX
+            if right_mask.sum() < self.valid_hand_seg_threshold:
+                seg_real[right_mask] = 0
+                right_energy.fill(0.0)
+        elif merge_mode >= 4 and merge_mode < 8:
+            # left hand bottom, right hand top
+            img_real, bg_img_resized = merge_hands(right_img, left_img, bg_img)
+            img_real_orig, _ = merge_hands(right_img_orig, left_img_orig, bg_img_resized, bg_resize=False)
+            seg_real = np.zeros((img_real.shape[:2]), dtype=np.uint8)
+            seg_real[left_seg] = LEFT_IDX
+            seg_real[right_seg] = RIGHT_IDX
+            # if left-hand's size is insufficient, drop it
+            left_mask = seg_real == LEFT_IDX
+            if left_mask.sum() < self.valid_hand_seg_threshold:
+                seg_real[left_mask] = 0
+                left_energy.fill(0.0)
+        elif merge_mode == 8:
+            # drop left hand, right hand only
+            img_real, bg_img_resized = merge_hands(right_img, None, bg_img)
+            img_real_orig, _ = merge_hands(right_img_orig, None, bg_img_resized, bg_resize=False)
+            seg_real = np.zeros((img_real.shape[:2]), dtype=np.uint8)
+            seg_real[right_seg] = RIGHT_IDX
+            left_energy.fill(0.0)
+        elif merge_hands == 9:
+            # drop right hand, left hand only
+            img_real, bg_img_resized = merge_hands(left_img, None, bg_img)
+            img_real_orig, _ = merge_hands(left_img_orig, None, bg_img_resized, bg_resize=False)
+            seg_real = np.zeros((img_real.shape[:2]), dtype=np.uint8)
+            seg_real[left_seg] = LEFT_IDX
+            right_energy.fill(0.0)
+
+        size_div_2 = [int(s / 2) for s in input_size]
+        size_div_4 = [int(s / 4) for s in input_size]
+        seg_real_div2 = ReScale(size_div_2, cv2.INTER_AREA)(seg_real)
+        seg_real_div4 = ReScale(size_div_4, cv2.INTER_AREA)(seg_real)
+
+        left_energy_div2 = ReScale(size_div_2, cv2.INTER_AREA)(left_energy)
+        left_energy_div4 = ReScale(size_div_4, cv2.INTER_AREA)(left_energy_div2)
+
+        right_energy_div2 = ReScale(size_div_2, cv2.INTER_AREA)(right_energy)
+        right_energy_div4 = ReScale(size_div_4, cv2.INTER_AREA)(right_energy_div2)
+
+        bg_energy = 1.0 - np.maximum(left_energy, right_energy)
+        bg_energy_div2 = 1.0 - np.maximum(left_energy_div2, right_energy_div2)
+        bg_energy_div4 = 1.0 - np.maximum(left_energy_div4, right_energy_div4)
+
+        energy_gt = np.stack([bg_energy, left_energy, right_energy], axis=0)
+        energy_gt2 = np.stack([bg_energy_div2, left_energy_div2, right_energy_div2], 0)
+        energy_gt4 = np.stack([bg_energy_div4, left_energy_div4, right_energy_div4], 0)
+
+        # 为何要转为灰度图
+        img_real = cv2.cvtColor(img_real, cv2.COLOR_RGB2GRAY)
+
+        if self.input_edge:
+            img_edge = cv2.Canny(img_real.astype(np.uint8), 25, 100).astype(np.float32)
+            img_real = np.stack([img_real, img_edge], axis=-1)
+        else:
+            img_real = np.expand_dims(img_real, axis=-1)
 
         image, label = self.generate_sample(left_src, left_seg)
         sample = {
             'image': image.copy(),
             'label': label.copy(),
-            'shape': np.array(shape),
         }
 
         return sample
@@ -74,7 +187,7 @@ class Ego2HandsData(BaseDataset):
 
         return samples
 
-    def generate_sample(self, image, label):
+    def generate_sample(self, objs):
         if self.transform is not None:
             image, label = self.transform(image, label)
 
@@ -88,3 +201,5 @@ class Ego2HandsAugment:
     def __call__(self, image, label):
 
         return image, label
+				
+
--- segmentron/data/dataloader/Ego2Hands_utils.py
+++ segmentron/data/dataloader/Ego2Hands_utils.py
@@ -1,9 +1,130 @@
 import cv2
 import os
 import random
+import math
 import numpy as np
 
+from segmentron.config import Cfg
 
+
+
+def get_sample_path_ego2hands(dataset_path_list, mode):
+    train_pairs = []
+    valid_pairs = []
+
+    img_path_list = []
+    seg_gt_path_list = []
+    energy_l_path_list = []
+    energy_r_path_list = []
+
+    if 'Train' in mode:
+        for dataset_path in dataset_path_list:
+
+            for root, dirs, files in os.walk(dataset_path):
+                for f in files:
+                    if f.endswith('.png') and "energy" not in f and "vis" not in f:
+                        image_path = os.path.join(root, f)
+                        label_path = image_path.replace('.png', '_energy.png')
+                        train_pairs.append((image_path, label_path))
+
+        return train_pairs
+
+    elif 'Valid' in mode:
+        for dataset_path in dataset_path_list:
+
+            for root, dirs, files in os.walk(dataset_path):
+                for f in files:
+                    if f.endswith(".png") and os.path.splitext(f)[0][-1].isdigit():
+                        image_path = os.path.join(root, f)
+                        label_path = os.path.join(root, f.replace(".png", "_seg.png"))
+                        # energy_l_path_list.append(os.path.join(root, f.replace(".png", "_e_l.png")))
+                        # energy_r_path_list.append(os.path.join(root, f.replace(".png", "_e_r.png")))
+                        valid_pairs.append((image_path, label_path))
+
+        return valid_pairs
+
+def read_bg_data(bg_path):
+    suffix = ['.jpg', '.png', '.jpeg']
+
+    bg_path_list = []
+    for root, dirs, files in os.walk(bg_path):
+        for f in files:
+            _, ext = os.path.splitext(f)
+            if ext in suffix:
+                bg_path_list.append(os.path.join(root, f))
+    return bg_path_list
+
+def random_bg_augment(img, bg_adapt=False, brightness_aug=True, flip_aug=False):
+    if brightness_aug:
+        if bg_adapt:
+            brightness_mean = int(np.mean(img))
+            brightness_val = random.randint(brightness_mean - 50, brightness_mean + 50)
+            img = random_brightness(img, None, brightness_val=brightness_val)
+        else:
+            brightness_val = random.randint(35, 220)
+            img = random_brightness(img, None, brightness_val=brightness_val)
+    if flip_aug:
+        do_flip = random.getrandbits(1)
+        if do_flip:
+            img = cv2.flip(img, 1)
+    return img
+
+def resize_bg(fg_shape, bg_img, bg_adapt):
+    fg_h, fg_w = fg_shape[:2]
+    if not bg_adapt:
+        bg_h, bg_w = bg_img.shape[:2]
+
+        if bg_h < fg_h or bg_w < fg_w:
+            h_ratio = float(fg_h / bg_h)
+            w_ratio = float(fg_w / bg_w)
+            ratio = max(h_ratio, w_ratio)
+
+            tar_w, tar_h = math.ceil(bg_w*ratio), math.ceil(bg_h*ratio)
+            bg_img = cv2.resize(bg_img, (tar_w, tar_h))
+
+        bg_h, bg_w = bg_img.shape[:2]
+
+        h_offset_range = max(bg_h - fg_h, 0)
+        w_offset_range = max(bg_w - fg_w, 0)
+        h_offset = random.randint(0, h_offset_range)
+        w_offset = random.randint(0, w_offset_range)
+        bg_img = bg_img[h_offset:h_offset+fg_h, w_offset:w_offset+fg_w, :3]
+    else:
+        bg_img = cv2.resize(bg_img, (fg_w, fg_h))
+    return bg_img
+
+def add_alpha_border(img):
+    assert img.shape[-1] > 3, "Image shuold has alpha channel."
+    fg_mask = (img[:, :, -1] == 0).astype(np.uint8)
+    fg_mask = cv2.dilate(fg_mask, np.ones((3, 3)))
+    alpha_mask = fg_mask * 255
+    alpha_mask = 255 - cv2.GaussianBlur(alpha_mask, (7, 7), 0)
+    img[:, :, -1] = alpha_mask
+    img_seg = alpha_mask > 200
+    img_all_seg = alpha_mask > 0
+    return img, img_seg, img_all_seg
+
+def add_alpha_image_to_bg(alpha_img, bg_img):
+    alpha = (alpha_img[:, :, -1] / 255.)[:, :, None]
+    alpha_s = np.repeat(alpha, 3, axis=2)
+    alpha_l = 1.0 - alpha_s
+    combined_img = np.multiply(alpha_s, alpha_img[:, :, :3]) + \
+                        np.multiply(alpha_l, bg_img)
+    return combined_img
+
+def merge_hands(top_hand_img, bot_hand_img, bg_img, bg_adapt=False, bg_resize=True):
+    assert top_hand_img is not None, 'There should be at least one hand to be merged.'
+    bg_img_resized = resize_bg(top_hand_img.shape, bg_img, bg_adapt) if bg_resize else bg_img
+
+    if bot_hand_img is not None:
+        top_hand_img, _, _ = add_alpha_border(top_hand_img)
+        bot_hand_img, _, _ = add_alpha_border(bot_hand_img)
+        final_hand_img = add_alpha_image_to_bg(bot_hand_img, bg_img_resized)
+        final_hand_img = add_alpha_image_to_bg(top_hand_img, final_hand_img)
+    else:
+        top_hand_img, _, _ = add_alpha_border(top_hand_img)
+        final_hand_img = add_alpha_image_to_bg(top_hand_img, bg_img_resized)
+    return final_hand_img, bg_img_resized
 
 def random_translation(img, seg, energy):
     img_h, img_w = img.shape[:2]
@@ -77,5 +198,54 @@ def random_translation(img, seg, energy):
     energy_[new_top:new_bot, new_left:new_right] = energy[old_top:old_bot, old_left:old_right]
 
     return img_, seg_, energy_
+
+def get_random_brightness(bg_adapt=False, custom=False, seq_i=-1):
+    dark_lighting_set = [5]
+    normal_lighting_set = [1, 3, 4, 6, 7]
+    bright_lighting_set = [2, 8]
+    brightness_map = {"dark": (0, 55), "normal": (55, 200), "bright": (55, 255)}
+    if not bg_adapt:
+        return random.randint(15, 240)
+    else:
+        if not custom:
+            if seq_i in dark_lighting_set:
+                return random.randint(*brightness_map["dark"])
+            elif seq_i in normal_lighting_set:
+                return random.randint(*brightness_map["normal"])
+            elif seq_i in bright_lighting_set:
+                return random.randint(*brightness_map["bright"])
+        else:
+            custom_scene_brightness = Cfg.AUGMENT.BRIGHTNESS_MODE
+            assert custom_scene_brightness != "", "Error: custom scene brightness not set. Please set \"custom_scene_brightness\" in the config file."
+            assert custom_scene_brightness in brightness_map, "Error: unrecognized brightness {} (valid options [\"dark\", \"normal\", \"bright\"]".format(config.custom_scene_brightness)
+            return random.randint(*brightness_map[custom_scene_brightness])
+
+def random_brightness(img, seg, magnitude=20, brightness_val=None):
+    brightness_val = get_random_brightness() if brightness_val is None else brightness_val
+
+    old_mean = np.mean(img[seg]) if seg is not None else np.mean(img)
+    assert old_mean != 0, "Image's mean value cannot be empty."
+    new_mean = brightness_val + random.uniform(-magnitude/2, magnitude/2)
+    img *= (new_mean / old_mean)
+    img = np.clip(img, 0, 255)
+    return img
+
+def random_smoothness(img, smooth_rate=0.3):
+    smooth_rate_tick = smooth_rate / 5
+    rand_val = random.random()
+    if rand_val < smooth_rate:
+        if rand_val < smooth_rate_tick:
+            kernel_size = 3
+        elif rand_val < smooth_rate_tick * 2:
+            kernel_size = 5
+        elif rand_val < smooth_rate_tick * 3:
+            kernel_size = 7
+        elif rand_val < smooth_rate_tick * 4:
+            kernel_size = 9
+        else:
+            kernel_size = 11
+        
+        img[:, :, :3] = cv2.blur(img[:, :, :3], (kernel_size, kernel_size))
+    return img
 
 
